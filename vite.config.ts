652c5911d24b21fe5261c754243a9fff9a9f0276
Abd. Rauf Jauhari Izza Mazzidan <raufmazzidan@gmail.com>
2025-06-06 20:10:33 +0700

    feat: add library builder

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 import { configDefaults } from "vitest/config";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), dts()],
   test: {
     ...configDefaults,
     globals: true,
     environment: "jsdom",
     setupFiles: "./setupTests.ts",
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "DesignSystemPlayground",
+      formats: ["es", "umd"],
+      fileName: (format) => `design-system-playgournd.${format}.js`,
+    },
   },
 });
